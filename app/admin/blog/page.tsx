62851bb4c4ecad53e943eb1a5fe9779e1a11e7d9
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2025-10-08 02:00:24 +0530

    Update page.tsx

--- app/admin/blog/page.tsx
+++ app/admin/blog/page.tsx
@@ -1,5 +1,6 @@
 
-"use client" import { useState, useEffect } from "react"
+"use client"
+import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { useIsAdmin } from "@/hooks/use-is-admin"
 import { Button } from "@/components/ui/button"
